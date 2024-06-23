8ebe0c8eab356d6abee2c5f115988e4b1ba8b648
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-06-23 23:33:06 +0300

    Connect and discover Oracle OLAP

--- olap-java-oracledb/src/main/java/acme/oracledb/metadata/ConnectAndDiscover.java
+++ olap-java-oracledb/src/main/java/acme/oracledb/metadata/ConnectAndDiscover.java
@@ -4,17 +4,21 @@ import acme.oracledb.BaseExample;
 import oracle.jdbc.OracleConnection;
 import oracle.jdbc.pool.OracleDataSource;
 import oracle.olapi.data.source.DataProvider;
-import oracle.olapi.metadata.mdm.MdmMetadataProvider;
+import oracle.olapi.metadata.deployment.AW;
+import oracle.olapi.metadata.mdm.*;
 import oracle.olapi.session.UserSession;
 import oracle.olapi.transaction.TransactionProvider;
 import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnectAndDiscover extends BaseExample {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(ConnectAndDiscover.class);
 
-    private boolean isGlobalAwjOnly;
+    private boolean isGlobalAwOnly;
+    private AW globalAw;
 
     @Override
     protected void run() throws Exception {
@@ -55,21 +59,130 @@ public class ConnectAndDiscover extends BaseExample {
         MdmMetadataProvider mp = null;
 
         try {
-            mp = (MdmMetadataProvider) dp.getMdmMetadataProvider();
+            mp = dp.getMdmMetadataProvider();
+
+            // Get only metadata objects for the GLOBAL schema.
+            // To get all the available metadata objects, specify false.
+            setGlobalAwOnly(true);
+
+            if (!isGlobalAwOnly) {
+                log.info("Getting the MdmSchema Objects.");
+            }
+
+            MdmDatabaseSchema mdmGlobalSchema = getSchemaAndPrintAllSchemas(mp, "GLOBAL");
+
+            // Get the GLOBAL analytic workspace.
+            setGlobalAw(mdmGlobalSchema, "GLOBAL");
+
+            if (!isGlobalAwOnly) {
+                log.info("Getting the Dimensions and Measures of an MdmDatabaseSchema.");
+            } else {
+                log.info("Getting the Dimensions and Measures of an AW.");
+            }
+
+            getObjects(mdmGlobalSchema);
+
+            log.info("Getting the Dimensions and Measures of an MdmCube.");
+            getCubeObjects(mdmGlobalSchema);
+
+            //TODO: implement
         } catch (Exception e) {
             log.error("Cannot get the MDM metadata provider. ", e);
         }
 
-        // Get only metadata objects for the GLOBAL schema.
-        // To get all the available metadata objects, specify false.
-        setGlobalAwjOnly(true);
+        // Now close the data provider and the connection.
+        log.info("Closing the DataProvider.");
+        dp.close();     // dp is the DataProvider
 
-        if (!isGlobalAwjOnly) {
-            log.info("Getting the MdmSchema Objects.");
+        log.info("Closing the Connection.");
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            log.info("Cannot close the connection. " + e);
         }
-//        MdmDatabaseSchema mdmGlobalSchema = getSchemas(mp);
+    }
 
-        //TODO: implement
+    private MdmDatabaseSchema getSchemaAndPrintAllSchemas(MdmMetadataProvider mp, String schemaName) {
+        MdmRootSchema mdmRootSchema = (MdmRootSchema) mp.getRootSchema();
+        List<MdmDatabaseSchema> dbSchemas = mdmRootSchema.getDatabaseSchemas();
+
+        if (!isGlobalAwOnly) {
+            for (MdmDatabaseSchema mdmDBSchema : dbSchemas) {
+                log.info(mdmDBSchema.getName());
+                printOrgSchemas(mdmDBSchema);
+            }
+        }
+
+        log.info("Getting a Single MdmDatabaseSchema.");
+
+        return mdmRootSchema.getDatabaseSchema(schemaName);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void printOrgSchemas(MdmSchema mdmSchema) {
+        List<MdmOrganizationalSchema> orgSchemaList = new ArrayList<>();
+
+        if (mdmSchema instanceof MdmDatabaseSchema mdmDBSchema) {
+            orgSchemaList = (List<MdmOrganizationalSchema>) mdmDBSchema.getOrganizationalSchemas();
+        } else if (mdmSchema instanceof MdmOrganizationalSchema mdmOrgSchema) {
+            orgSchemaList = (List<MdmOrganizationalSchema>) mdmOrgSchema.getOrganizationalSchemas();
+        }
+
+        if (orgSchemaList.size() > 0) {
+            log.info("The MdmOrganizationalSchema subschemas of {} are:", mdmSchema.getName());
+
+            for (MdmOrganizationalSchema mdmOrgSchema : orgSchemaList) {
+                log.info(mdmOrgSchema.getName());
+                printOrgSchemas(mdmOrgSchema);
+            }
+        } else {
+            log.info("{} does not have any MdmOrganizationalSchema subschemas.", mdmSchema.getName());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void getObjects(MdmDatabaseSchema mdmGlobalSchema) {
+        if (!isGlobalAwOnly) {
+            List<MdmObject> dimList = mdmGlobalSchema.getDimensions();
+            String objName = mdmGlobalSchema.getName() + " schema";
+            getNames(dimList, "dimensions", objName);
+
+            List<MdmObject> measList = mdmGlobalSchema.getMeasures();
+            getNames(measList, "measures", objName);
+        } else {
+            List<MdmObject> dimList = globalAw.getDimensions();
+            String objName = globalAw.getName() + " analytic workspace";
+            getNames(dimList, "dimensions", objName);
+
+            List<MdmCube> cubeList = globalAw.getCubes();
+            ArrayList<MdmMeasure> allMeasures = new ArrayList<>();
+            for (MdmCube mdmCube : cubeList) {
+                List<MdmMeasure> measList = mdmCube.getMeasures();
+                allMeasures.addAll(measList);
+            }
+            getNames(allMeasures, "measures", objName);
+        }
+    }
+
+    private void getNames(List<? extends MdmObject> objectList, String objTypes, String objName) {
+        log.info("The {} of the {} are:", objTypes, objName);
+
+        for (MdmObject mdmObject : objectList) {
+            log.info("   {}", mdmObject.getName());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void getCubeObjects(MdmDatabaseSchema mdmGlobalSchema) {
+        MdmCube mdmUnitsCube = (MdmCube) mdmGlobalSchema.getTopLevelObject("PRICE_CUBE");
+        String objName = mdmUnitsCube.getName() + " cube";
+        List<MdmObject> dimList = mdmUnitsCube.getDimensions();
+        getNames(dimList, "dimensions", objName);
+
+        List<MdmMeasure> measList = mdmUnitsCube.getMeasures();
+        getNames(measList, "measures", objName);
     }
 
     public void execute(String[] args) {
@@ -77,8 +190,19 @@ public class ConnectAndDiscover extends BaseExample {
         super.execute(convertToProperties(args));
     }
 
-    public void setGlobalAwjOnly(boolean isGlobalAwjOnly) {
-        this.isGlobalAwjOnly = isGlobalAwjOnly;
+    public void setGlobalAwOnly(boolean isGlobalAwjOnly) {
+        this.isGlobalAwOnly = isGlobalAwjOnly;
+    }
+
+    private void setGlobalAw(MdmDatabaseSchema mdmGlobalSchema, String awName) {
+        List<AW> aws = mdmGlobalSchema.getAWs();
+
+        for (AW aw : aws) {
+            if (aw.getName().equals(awName)) {
+                globalAw = aw;
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
